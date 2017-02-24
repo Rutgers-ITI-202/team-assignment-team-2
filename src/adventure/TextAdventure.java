7cdaa5cee20d11869f98222dd850592711b3c44a
aciaramello6 <aciaramello6@gmail.com>
2017-02-24 15:56:43 -0500

    TextAdventure
    Collaboratively worked on in eclipse due to initial GitHub setup issues.

--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -4,13 +4,47 @@ This class:
 •	Talks to the AdventureModel and to the Adventurer classes, as needed.
 */
 
-package adventure;
-
+import java.util.*;
 public class TextAdventure {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int location = 0;
+	public static Scanner sc;
+	
+	public static void main(String[] args){
+		
+		sc = new Scanner(System.in);
+		AdventureModel.startGame();
+		
+		
+		//To show player's location --- Bunks programmed in part two.
+		System.out.println("To the left is the door to the bunks. Enter 1 to\n"
+				+ "stay in the command room, and 2 to enter the bunks. \n\n> ");
 
+		
+		
+		int intInput = sc.nextInt();
+		
+		
+		
+		//switch statement for room location
+		switch(intInput){
+				case 1:
+					location = 1;
+					AdventureModel.commandRoom();
+					//shows item is in inventory
+					Adventurer.showInventory();
+					break;
+				case 2:
+					System.out.println("This part is not programmed yet\n\n");
+					break;
+				default: System.out.println("You have entered an invald command\n\n");
+				
+		}
+		
 	}
-
+	
+	
 }
+
+
+
